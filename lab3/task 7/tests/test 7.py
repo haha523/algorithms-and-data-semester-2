d1d712c83e5d8aa3265f5b429ffa49409aa9c2ef
Хыу Жанг haha234@ <77500794+haha523@users.noreply.github.com>
2025-03-31 17:32:44 +0300

    Update

--- lab3/task 7/tests/test 7.py
+++ lab3/task 7/tests/test 7.py
@@ -1,1 +1,30 @@
+import unittest
+import os
+import sys
 
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
+from c7 import task_scheduler
+
+class TestBipartiteCheck(unittest.TestCase):
+
+    def test_bipartite_graph(self):
+        # given
+        input_file = os.path.join('..', 'txtf', 'input.txt')
+        output_file = os.path.join('..', 'txtf', 'output.txt')
+
+        # when
+        with open(input_file, 'r') as f:
+            n, m = map(int, f.readline().strip().split())
+            edges = [tuple(map(int, f.readline().strip().split())) for _ in range(m)]
+
+        result = task_scheduler(input_file='input.txt', output_file='output.txt')
+
+        with open(output_file, 'r') as f:
+            expected_output = f.read().strip()
+
+        # then
+        self.assertIn(result, ['0', '1'])
+        self.assertEqual(result, expected_output)
+
+if __name__ == "__main__":
+    unittest.main()
