b9d1e7fb0b1f1b2bca267c7c4aa72bae80aceea9
Хыу Жанг haha234@ <77500794+haha523@users.noreply.github.com>
2025-03-31 17:13:11 +0300

    Update

--- lab3/task 2/tests/test time 2.py
+++ lab3/task 2/tests/test time 2.py
@@ -1,1 +1,28 @@
+import sys
+import time
+import os
 
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
+from c2 import count_components
+
+def measure_time_and_memory(input_file, output_file):
+
+    start_time = time.time()
+    result = count_components(input_file=input_file, output_file=output_file, to_file=False)
+    end_time = time.time()
+    execution_time = end_time - start_time
+    total_size = sys.getsizeof(result)
+    total_size += sys.getsizeof(input_file) + sys.getsizeof(output_file)
+
+    print(f"Количество подключенных компонентов: {result}")
+    print(f"Общий размер памяти: {total_size} байт")
+    print(f"Время выполнения: {execution_time:.6f} секунд")
+
+if __name__ == "__main__":
+    input_file = '../txtf/input.txt'
+    output_file = '../txtf/output.txt'
+
+    with open(input_file, 'w') as f:
+        f.write("4 2\n1 2\n3 2\n")
+
+    measure_time_and_memory(input_file, output_file)
